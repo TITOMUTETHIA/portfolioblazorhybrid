9216f0da599300fda359edfa5e1b9fb2148837f2
Tito Mutethia <titomutethia@gmail.com>
2026-07-30 21:31:23 +0300

    ignore build artifacts

--- obj/Debug/net10.0-windows10.0.19041.0/win-x64/Portfolio.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows10.0.19041.0/win-x64/Portfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73e8488d48f0feacdae4dcbde977e373b2412429")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
